60ddddaf22ea139d85fa6c0fb0e8d361c1d52dd0
Érky Lima <erkylima@gmail.com>
2024-07-29 12:41:14 -0300

    ✨ added format to logging

--- tasks.py
+++ tasks.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     handler_chain = LATimesScraperNewsHandler(ExcelSaverHandler())
-    logging.basicConfig(filename='output/extraction.log', level=logging.INFO)
+    logging.basicConfig(filename='output/extraction.log', level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
     search_term = os.environ.get('SEARCH_TERM', '')
     filter = os.environ.get('FILTER', 'Newsletter')
